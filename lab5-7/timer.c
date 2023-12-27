e32b5382518e8ba05a2081a5fd8b414e4c4f07ca
Roman <prime.zen2017@yandex.ru>
2023-12-27 05:41:01 +0300

    Update timer

--- lab5-7/timer.c
+++ lab5-7/timer.c
@@ -1,73 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/time.h>
+#include <string.h>
 
 typedef struct {
-    int status;
-    struct timeval start_time, stop_time;
-} timer;
+    int is_running;
+    struct timeval start_time;
+    long elapsed_time;
+} Timer;
 
-timer* init() {
-    timer *newT = (timer*)malloc(sizeof(timer));
-    newT->status = 0;
-
-    return newT;
+Timer *createTimer() {
+    Timer *newTimer = (Timer*)malloc(sizeof(Timer));
+    newTimer->is_running = 0;
+    newTimer->elapsed_time = 0;
+    return newTimer;
 }
 
-void startTimer(timer *t) {
-    if (t->status == 1) {
-        printf("Ошибка: таймер уже запущен\n");
-        return;
-    }
-    gettimeofday(&t->start_time, NULL);
-    t->status = 1;
-    printf("Таймер запущен\n");
-}
-
-void stopTimer(timer *t) {
-    if (t->status == 0) {
-        printf("Ошибка: таймер уже остановлен\n");
-        return;
-    }
-    gettimeofday(&t->stop_time, NULL);
-    t->status = 0;
-    printf("Таймер остановлен\n");
-}
-
-double getTime(timer *t) {
+long getCurrentTime() {
     struct timeval current_time;
     gettimeofday(&current_time, NULL);
+    return (current_time.tv_sec * 1000) + (current_time.tv_usec / 1000);
+}
 
-    long seconds = current_time.tv_sec - t->start_time.tv_sec;
-    long microseconds = current_time.tv_usec - t->start_time.tv_usec;
-    double elapsed_time = seconds + microseconds / 1000000.0;
-    return elapsed_time * 1000;
+void startTimer(Timer* timer) {
+    if (timer != NULL && !timer->is_running) {
+        timer->is_running = 1;
+        gettimeofday(&timer->start_time, NULL);
+        printf("Ok: start\n");
+    } else {
+        printf("Ошибка: недопустимый идентификатор таймера или таймер уже запущен\n");
+    }
+}
+
+void stopTimer(Timer* timer) {
+    if (timer != NULL && timer->is_running) {
+        timer->is_running = 0;
+        long elapsed_time = getCurrentTime() - (timer->start_time.tv_sec * 1000 + timer->start_time.tv_usec / 1000);
+        timer->elapsed_time += elapsed_time;
+        printf("Ok: stop\n");
+    } else {
+        printf("Ошибка: недопустимый идентификатор таймера или таймер уже остановлен\n");
+    }
+}
+
+void showTime(Timer* timer) {
+    if (timer != NULL) {
+        long elapsed_time = timer->elapsed_time;
+
+        if (timer->is_running) {
+            elapsed_time += getCurrentTime() - (timer->start_time.tv_sec * 1000 + timer->start_time.tv_usec / 1000);
+        }
+
+        printf("Ok: time: %ld\n", elapsed_time);
+    } else {
+        printf("Ошибка: недопустимый идентификатор таймера\n");
+    }
 }
 
 int main() {
-    timer *t = init();
+    Timer *timer = createTimer();
+    char command[10];
 
-    char cmd[10];
+    while (1) {
+        printf("> ");
+        scanf("%s", command);
 
-    while(1) {
-        printf("Введите команду (start, stop, time): ");
-        scanf("%s", cmd);
+        if (strcmp(command, "exec") == 0) {
+            scanf("%s", command);
 
-        if (strcmp(cmd, "start") == 0) {
-            startTimer(t);
-        }
-        else if (strcmp(cmd, "stop") == 0) {
-            stopTimer(t);
-        }
-        else if (strcmp(cmd, "time") == 0) {
-            if (t->status) {
-                printf("Текущее прошедшее время: %.3f миллисекунд\n", getTime(t));
+            if (strcmp(command, "start") == 0) {
+                startTimer(timer);
+            } else if (strcmp(command, "stop") == 0) {
+                stopTimer(timer);
+            } else if (strcmp(command, "time") == 0) {
+                showTime(timer);
+            } else {
+                printf("Ошибка: недопустимая команда\n");
             }
-            else {
-                printf("Таймер остановлен\n");
-            }
+        } else {
+            printf("Ошибка: недопустимая команда\n");
         }
     }
+
     return 0;
 }
